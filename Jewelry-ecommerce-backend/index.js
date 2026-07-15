18a3a41b8422840b0b4f698947ebb61c63b0f803
VaishnavAjish <vaishnavajish23@gmail.com>
2026-07-15 17:27:24 +0530

    fix: increase express body parser limit to fix 413 error

--- Jewelry-ecommerce-backend/index.js
+++ Jewelry-ecommerce-backend/index.js
@@ -28,7 +28,8 @@ const clientRoutes = require("./routes/client.routes");
 
 // middleware
 app.use(cors());
-app.use(express.json());
+app.use(express.json({ limit: '50mb' }));
+app.use(express.urlencoded({ limit: '50mb', extended: true }));
 app.use(morgan('dev'));
 app.use(express.static(path.join(__dirname, 'public')));
 
