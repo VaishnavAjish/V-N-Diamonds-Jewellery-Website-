60c13acc495a98bee3d2aa1d425ace4fee08af58
VaishnavAjish <vaishnavajish23@gmail.com>
2026-07-17 22:03:07 +0530

    Reduce spacing below top bar

--- Jewelry-ecommerce-client-main/src/layout/headers/header-4.jsx
+++ Jewelry-ecommerce-client-main/src/layout/headers/header-4.jsx
@@ -53,7 +53,7 @@ const HeaderFour = () => {
       <header>
         <div id="header-sticky" className={`tp-header-area ${isMinimalDesign ? '' : 'tp-header-style-transparent-white'} tp-header-sticky tp-header-transparent has-dark-logo tp-header-height ${sticky ? 'header-sticky' : ''}`}>
           {/* NEW TOP BAR */}
-          <div style={{ backgroundColor: 'transparent', padding: '2px 0' }} className="d-none d-md-block">
+          <div style={{ backgroundColor: 'transparent', padding: '0', marginBottom: '-15px', position: 'relative', zIndex: 99 }} className="d-none d-md-block">
             <div className="container-fluid pl-85 pr-85">
               <div className="row">
                 <div className="col-12 d-flex justify-content-center align-items-center" style={{ color: sticky ? '#000' : '#fff', fontSize: '13px', fontWeight: 500, letterSpacing: '0.5px' }}>
